8cd8c8ea36a19c32152860f6e37ae397986d4cd1
Andre Carrera <acarrera94@gmail.com>
2017-07-22 17:37:19 -0600

    init commit

---
+++ Restaurants/main.cpp
@@ -0,0 +1,653 @@
+/*
+ Andre Carrera
+ TEST CASES!!!!
+ 
+ Select One of the Following Options:
+ 1 - Display all restaurants
+ 2 - Add a restaurant
+ 3 - Remove a restaurant
+ 4 - Shuffle the vector
+ 5 - Begin the tournament
+ 6 - Quit the program
+ 1
+ DISPLAY OF ALL THE RESTAURANTS
+ T.G.I. Fridays,Olive Garden,Red Robin's,The Cheesecake Factory,California Pizza Kitchen,The Pi Pizzaria,Red Iguana,P.F. Chang's
+ 
+ Select One of the Following Options:
+ 1 - Display all restaurants
+ 2 - Add a restaurant
+ 3 - Remove a restaurant
+ 4 - Shuffle the vector
+ 5 - Begin the tournament
+ 6 - Quit the program
+ 2
+ What is the name of the restaurant to add?
+ 
+ Burger King
+ Burger King Has been added to the list.
+ 
+ Select One of the Following Options:
+ 1 - Display all restaurants
+ 2 - Add a restaurant
+ 3 - Remove a restaurant
+ 4 - Shuffle the vector
+ 5 - Begin the tournament
+ 6 - Quit the program
+ 3
+ What is the name of the restaurant to remove?
+ 
+ BurgerKing
+ The restaurant is not on the list, nothing has been removed.
+ Select One of the Following Options:
+ 1 - Display all restaurants
+ 2 - Add a restaurant
+ 3 - Remove a restaurant
+ 4 - Shuffle the vector
+ 5 - Begin the tournament
+ 6 - Quit the program
+ 3
+ What is the name of the restaurant to remove?
+ 
+ Burger
+ The restaurant is not on the list, nothing has been removed.
+ Select One of the Following Options:
+ 1 - Display all restaurants
+ 2 - Add a restaurant
+ 3 - Remove a restaurant
+ 4 - Shuffle the vector
+ 5 - Begin the tournament
+ 6 - Quit the program
+ 5
+ No tournament! The number of restaurants is not a power of 2
+ Select One of the Following Options:
+ 1 - Display all restaurants
+ 2 - Add a restaurant
+ 3 - Remove a restaurant
+ 4 - Shuffle the vector
+ 5 - Begin the tournament
+ 6 - Quit the program
+ 3
+ What is the name of the restaurant to remove?
+ 
+ Burger King
+ The restaurant, Burger King, has been erased.
+ 
+ Select One of the Following Options:
+ 1 - Display all restaurants
+ 2 - Add a restaurant
+ 3 - Remove a restaurant
+ 4 - Shuffle the vector
+ 5 - Begin the tournament
+ 6 - Quit the program
+ 4
+ The Restaurants have been shuffled.
+ 
+ Select One of the Following Options:
+ 1 - Display all restaurants
+ 2 - Add a restaurant
+ 3 - Remove a restaurant
+ 4 - Shuffle the vector
+ 5 - Begin the tournament
+ 6 - Quit the program
+ 1
+ DISPLAY OF ALL THE RESTAURANTS
+ The Cheesecake Factory,Red Robin's,P.F. Chang's,The Pi Pizzaria,California Pizza Kitchen,T.G.I. Fridays,Red Iguana,Olive Garden
+ 
+ Select One of the Following Options:
+ 1 - Display all restaurants
+ 2 - Add a restaurant
+ 3 - Remove a restaurant
+ 4 - Shuffle the vector
+ 5 - Begin the tournament
+ 6 - Quit the program
+ 5
+ TOURNAMENT
+ Match 1/4, Round 1/3 --- 1: The Cheesecake Factory or 2: Red Robin's? 1
+ Match 2/4, Round 1/3 --- 1: P.F. Chang's or 2: The Pi Pizzaria? 1
+ Match 3/4, Round 1/3 --- 1: California Pizza Kitchen or 2: T.G.I. Fridays? t
+ Invalid response (t)
+ Match 3/4, Round 1/3 --- 1: California Pizza Kitchen or 2: T.G.I. Fridays? 2
+ Match 4/4, Round 1/3 --- 1: Red Iguana or 2: Olive Garden? 1
+ Match 1/2, Round 2/3 --- 1: The Cheesecake Factory or 2: P.F. Chang's? 2
+ Match 2/2, Round 2/3 --- 1: T.G.I. Fridays or 2: Red Iguana? 1
+ Match 1/1, Round 3/3 --- 1: P.F. Chang's or 2: T.G.I. Fridays? 1
+ P.F. Chang's is the winner!
+ 
+ 
+ 
+ Select One of the Following Options:
+ 1 - Display all restaurants
+ 2 - Add a restaurant
+ 3 - Remove a restaurant
+ 4 - Shuffle the vector
+ 5 - Begin the tournament
+ 6 - Quit the program
+ 1
+ DISPLAY OF ALL THE RESTAURANTS
+ T.G.I. Fridays,Olive Garden,Red Robin's,The Cheesecake Factory,California Pizza Kitchen,The Pi Pizzaria,Red Iguana,P.F. Chang's
+ 
+ Select One of the Following Options:
+ 1 - Display all restaurants
+ 2 - Add a restaurant
+ 3 - Remove a restaurant
+ 4 - Shuffle the vector
+ 5 - Begin the tournament
+ 6 - Quit the program
+ 3
+ What is the name of the restaurant to remove?
+ 
+ T.G.I. Fridays
+ The restaurant, T.G.I. Fridays, has been erased.
+ 
+ Select One of the Following Options:
+ 1 - Display all restaurants
+ 2 - Add a restaurant
+ 3 - Remove a restaurant
+ 4 - Shuffle the vector
+ 5 - Begin the tournament
+ 6 - Quit the program
+ 2
+ What is the name of the restaurant to add?
+ 
+ Rancheritos
+ Rancheritos Has been added to the list.
+ 
+ Select One of the Following Options:
+ 1 - Display all restaurants
+ 2 - Add a restaurant
+ 3 - Remove a restaurant
+ 4 - Shuffle the vector
+ 5 - Begin the tournament
+ 6 - Quit the program
+ 4
+ The Restaurants have been shuffled.
+ 
+ Select One of the Following Options:
+ 1 - Display all restaurants
+ 2 - Add a restaurant
+ 3 - Remove a restaurant
+ 4 - Shuffle the vector
+ 5 - Begin the tournament
+ 6 - Quit the program
+ 1
+ DISPLAY OF ALL THE RESTAURANTS
+ The Pi Pizzaria,California Pizza Kitchen,Olive Garden,Red Robin's,The Cheesecake Factory,Rancheritos,P.F. Chang's,Red Iguana
+ 
+ Select One of the Following Options:
+ 1 - Display all restaurants
+ 2 - Add a restaurant
+ 3 - Remove a restaurant
+ 4 - Shuffle the vector
+ 5 - Begin the tournament
+ 6 - Quit the program
+ 5
+ TOURNAMENT
+ Match 1/4, Round 1/3 --- 1: The Pi Pizzaria or 2: California Pizza Kitchen? 1
+ Match 2/4, Round 1/3 --- 1: Olive Garden or 2: Red Robin's? 1
+ Match 3/4, Round 1/3 --- 1: The Cheesecake Factory or 2: Rancheritos? 2
+ Match 4/4, Round 1/3 --- 1: P.F. Chang's or 2: Red Iguana? 2
+ Match 1/2, Round 2/3 --- 1: The Pi Pizzaria or 2: Olive Garden? 1
+ Match 2/2, Round 2/3 --- 1: Rancheritos or 2: Red Iguana? 1
+ Match 1/1, Round 3/3 --- 1: The Pi Pizzaria or 2: Rancheritos? 2
+ Rancheritos is the winner!
+ 
+ 
+ 
+ Select One of the Following Options:
+ 1 - Display all restaurants
+ 2 - Add a restaurant
+ 3 - Remove a restaurant
+ 4 - Shuffle the vector
+ 5 - Begin the tournament
+ 6 - Quit the program
+ 1
+ DISPLAY OF ALL THE RESTAURANTS
+ T.G.I. Fridays,Olive Garden,Red Robin's,The Cheesecake Factory,California Pizza Kitchen,The Pi Pizzaria,Red Iguana,P.F. Chang's
+ 
+ Select One of the Following Options:
+ 1 - Display all restaurants
+ 2 - Add a restaurant
+ 3 - Remove a restaurant
+ 4 - Shuffle the vector
+ 5 - Begin the tournament
+ 6 - Quit the program
+ 4
+ The Restaurants have been shuffled.
+ 
+ Select One of the Following Options:
+ 1 - Display all restaurants
+ 2 - Add a restaurant
+ 3 - Remove a restaurant
+ 4 - Shuffle the vector
+ 5 - Begin the tournament
+ 6 - Quit the program
+ 1
+ DISPLAY OF ALL THE RESTAURANTS
+ The Cheesecake Factory,Red Robin's,Olive Garden,California Pizza Kitchen,P.F. Chang's,The Pi Pizzaria,Red Iguana,T.G.I. Fridays
+ 
+ Select One of the Following Options:
+ 1 - Display all restaurants
+ 2 - Add a restaurant
+ 3 - Remove a restaurant
+ 4 - Shuffle the vector
+ 5 - Begin the tournament
+ 6 - Quit the program
+ 2
+ What is the name of the restaurant to add?
+ 
+ Jack In The Box
+ Jack In The Box Has been added to the list.
+ 
+ Select One of the Following Options:
+ 1 - Display all restaurants
+ 2 - Add a restaurant
+ 3 - Remove a restaurant
+ 4 - Shuffle the vector
+ 5 - Begin the tournament
+ 6 - Quit the program
+ 1
+ DISPLAY OF ALL THE RESTAURANTS
+ The Cheesecake Factory,Red Robin's,Olive Garden,California Pizza Kitchen,P.F. Chang's,The Pi Pizzaria,Red Iguana,T.G.I. Fridays,Jack In The Box
+ 
+ Select One of the Following Options:
+ 1 - Display all restaurants
+ 2 - Add a restaurant
+ 3 - Remove a restaurant
+ 4 - Shuffle the vector
+ 5 - Begin the tournament
+ 6 - Quit the program
+ 3
+ What is the name of the restaurant to remove?
+ 
+ Red Iguana
+ The restaurant, Red Iguana, has been erased.
+ 
+ Select One of the Following Options:
+ 1 - Display all restaurants
+ 2 - Add a restaurant
+ 3 - Remove a restaurant
+ 4 - Shuffle the vector
+ 5 - Begin the tournament
+ 6 - Quit the program
+ 3
+ What is the name of the restaurant to remove?
+ 
+ The Cheesecake Factory
+ The restaurant, The Cheesecake Factory, has been erased.
+ 
+ Select One of the Following Options:
+ 1 - Display all restaurants
+ 2 - Add a restaurant
+ 3 - Remove a restaurant
+ 4 - Shuffle the vector
+ 5 - Begin the tournament
+ 6 - Quit the program
+ 3
+ What is the name of the restaurant to remove?
+ 
+ P.F. Chang's
+ The restaurant, P.F. Chang's, has been erased.
+ 
+ Select One of the Following Options:
+ 1 - Display all restaurants
+ 2 - Add a restaurant
+ 3 - Remove a restaurant
+ 4 - Shuffle the vector
+ 5 - Begin the tournament
+ 6 - Quit the program
+ 3
+ What is the name of the restaurant to remove?
+ 
+ Jack In The Box
+ The restaurant, Jack In The Box, has been erased.
+ 
+ Select One of the Following Options:
+ 1 - Display all restaurants
+ 2 - Add a restaurant
+ 3 - Remove a restaurant
+ 4 - Shuffle the vector
+ 5 - Begin the tournament
+ 6 - Quit the program
+ 3
+ What is the name of the restaurant to remove?
+ 
+ California Pizza Kitchen
+ The restaurant, California Pizza Kitchen, has been erased.
+ 
+ Select One of the Following Options:
+ 1 - Display all restaurants
+ 2 - Add a restaurant
+ 3 - Remove a restaurant
+ 4 - Shuffle the vector
+ 5 - Begin the tournament
+ 6 - Quit the program
+ 1
+ DISPLAY OF ALL THE RESTAURANTS
+ Red Robin's,Olive Garden,The Pi Pizzaria,T.G.I. Fridays
+ 
+ Select One of the Following Options:
+ 1 - Display all restaurants
+ 2 - Add a restaurant
+ 3 - Remove a restaurant
+ 4 - Shuffle the vector
+ 5 - Begin the tournament
+ 6 - Quit the program
+ 4
+ The Restaurants have been shuffled.
+ 
+ Select One of the Following Options:
+ 1 - Display all restaurants
+ 2 - Add a restaurant
+ 3 - Remove a restaurant
+ 4 - Shuffle the vector
+ 5 - Begin the tournament
+ 6 - Quit the program
+ 1
+ DISPLAY OF ALL THE RESTAURANTS
+ Olive Garden,Red Robin's,T.G.I. Fridays,The Pi Pizzaria
+ 
+ Select One of the Following Options:
+ 1 - Display all restaurants
+ 2 - Add a restaurant
+ 3 - Remove a restaurant
+ 4 - Shuffle the vector
+ 5 - Begin the tournament
+ 6 - Quit the program
+ 5
+ TOURNAMENT
+ Match 1/2, Round 1/2 --- 1: Olive Garden or 2: Red Robin's? 1
+ Match 2/2, Round 1/2 --- 1: T.G.I. Fridays or 2: The Pi Pizzaria? 2
+ Match 1/1, Round 2/2 --- 1: Olive Garden or 2: The Pi Pizzaria? 2
+ The Pi Pizzaria is the winner!
+ */
+
+
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <cmath>
+using namespace std;
+
+const int START = 0;
+
+void initRestaurants (vector<string>  & restaurants);
+
+void printmenu();
+
+void displayRestaurants (vector<string>  restaurants);
+
+int findRestaurant (vector<string>  & restaurants, string nameInput);
+
+void shuffleFunction (vector<string>  & restaurants, vector<string>  & restCopy);
+
+void addFunction ( vector<string>  & restaurants, int  (*findRestaurant)(vector<string>  & restaurants, string nameInput));
+
+void removeFunction ( vector<string>  & restaurants, int  (*findRestaurant)(vector<string>  & restaurants, string nameInput));
+
+void tournamentFunction ( vector<string>  & restaurants, double checkPow);
+
+
+int main() {
+    
+    vector<string> restaurants;
+    
+    int option = 0;
+    const int PRINT = 1;
+    const int ADD = 2;
+    const int REMOVE = 3;
+    const int SHUFFLE = 4;
+    const int TOURNAMENT = 5;
+    const int QUIT = 6;
+    srand( (unsigned int) time(0) );
+    
+    initRestaurants (restaurants);
+    
+    
+    while (true) {
+    printmenu();
+        cin >> option;
+        
+        if (option == PRINT)
+        {
+            cout << "DISPLAY OF ALL THE RESTAURANTS" << endl;
+            displayRestaurants (restaurants);
+        }
+        else if (option == ADD)
+        {
+
+            addFunction (restaurants, findRestaurant);
+            
+        }
+        else if (option == REMOVE)
+        {
+
+            removeFunction(restaurants, findRestaurant);
+            
+        }
+        else if (option == SHUFFLE){
+            
+            vector<string> restCopy;
+            shuffleFunction (restaurants, restCopy);
+        }
+        
+        else if (option == TOURNAMENT){
+           double checkPow = log2(restaurants.size());
+            
+            if ( checkPow != (int) checkPow)
+            {
+                cout << " No tournament! The number of restaurants is not a power of 2" << endl;
+            }
+            else if (checkPow == (int) checkPow)
+            {
+                tournamentFunction (restaurants, checkPow);
+                break;
+                    
+            }
+        }
+        
+        
+        else if (option == QUIT ) {
+            cout << "Quiting The program" << endl;
+            break;
+        }
+        else if (cin.fail() || option > QUIT || option < QUIT)
+        {
+            cout << "Invalid response" << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(),'\n');
+        }
+
+    }
+
+    
+    return 0;
+}
+
+void initRestaurants (vector<string>  & restaurants)
+{
+    restaurants.push_back("T.G.I. Fridays");
+    restaurants.push_back("Olive Garden");
+    restaurants.push_back("Red Robin's");
+    restaurants.push_back("The Cheesecake Factory");
+    restaurants.push_back("California Pizza Kitchen");
+    restaurants.push_back("The Pi Pizzaria");
+    restaurants.push_back("Red Iguana");
+    restaurants.push_back("P.F. Chang's");
+    
+}
+void printmenu()
+{
+    cout << "Select One of the Following Options:" << endl;
+    cout << "1 - Display all restaurants" << endl;
+    cout << "2 - Add a restaurant" << endl;
+    cout << "3 - Remove a restaurant" << endl;
+    cout << "4 - Shuffle the vector" << endl;
+    cout << "5 - Begin the tournament" << endl;
+    cout << "6 - Quit the program" << endl;
+}
+void displayRestaurants (vector<string>  restaurants)
+{
+    for (int counter = START; counter < restaurants.size(); counter++)
+    {
+        cout << restaurants[counter];
+        int lastComma = 1;
+        if (counter < restaurants.size() - lastComma )
+        {
+            cout << ",";
+        }
+        
+    }
+    cout << endl << endl;
+}
+
+int findRestaurant (vector<string>  & restaurants, string nameInput)
+{
+    int index = -1;
+    
+    for (int findCounter = 0; findCounter < restaurants.size() ; findCounter++)
+    {
+        if (restaurants[findCounter] == nameInput)
+        {
+            index = findCounter;
+            break;
+        }
+        
+    }
+    
+    return index;
+    
+    
+}
+void shuffleFunction (vector<string>  & restaurants, vector<string>  & restCopy)
+{
+    restCopy = restaurants;
+    
+    
+    for (int numOfSwaps = 0; numOfSwaps < restaurants.size(); numOfSwaps++) {
+        int firstPos = rand() % restaurants.size();
+        int secondPos = (rand() * rand()) % restaurants.size();
+        while (true) {//makes sure they aren't equal to each other.
+            
+            if (firstPos == secondPos)
+            {
+                secondPos = rand() % restaurants.size();
+            }
+            else if (firstPos != secondPos)
+            {
+                break;
+            }
+            
+        }
+        restaurants[firstPos] = restCopy [secondPos];
+        restaurants[secondPos] = restCopy[firstPos];
+        restCopy = restaurants;
+    }
+    cout << "The Restaurants have been shuffled." << endl <<endl;
+}
+void addFunction ( vector<string>  & restaurants, int  (*findRestaurant)(vector<string>  & restaurants, string nameInput))
+{
+    cout << "What is the name of the restaurant to add?" << endl << endl;
+    
+    string nameInput;
+    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    
+    getline(cin, nameInput);
+    
+    int found = (*findRestaurant) (restaurants, nameInput);
+    int notfound = -1;
+    
+    if (found == notfound) {
+        restaurants.push_back(nameInput);
+        int lastRest = 1;
+        cout << restaurants [restaurants.size() - lastRest] << " Has been added to the list." << endl << endl;
+    }
+    else {
+        
+        cout << "The restaurant is already in the list. Nothing has been added." << endl << endl;
+        
+    }
+    
+}
+void removeFunction ( vector<string>  & restaurants, int  (*findRestaurant)(vector<string>  & restaurants, string nameInput))
+{
+    cout << "What is the name of the restaurant to remove?" << endl << endl;
+    
+    string nameInput;
+    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    
+    getline(cin, nameInput);
+    
+    int found = (*findRestaurant) (restaurants, nameInput);
+    int notfound = -1;
+    
+    if (found == notfound) {
+        cout << "The restaurant is not on the list, nothing has been removed." << endl;
+        
+    }
+    else {
+        cout << "The restaurant, " << restaurants [found];
+        restaurants.erase(restaurants.begin() + found);
+        cout << ", has been erased." << endl << endl;
+        
+        
+    }
+    
+}
+void tournamentFunction ( vector<string>  & restaurants, double checkPow)
+{
+    cout << "TOURNAMENT" << endl;// here keep going
+    int getHalf = 2;
+    double toInt = 1.0;
+    int matchSize = (restaurants.size()/ getHalf)* toInt;
+    for (int round = 0 ; round < checkPow; round++) {
+        const int DISPLAYNEXT = 1;
+        
+        for (int counter = 0; counter < restaurants.size(); counter++) { // for each match
+            
+            
+            
+            
+            while (true) {
+                cout << "Match " << counter + DISPLAYNEXT<< "/" << matchSize
+                << ", Round " << round +DISPLAYNEXT <<"/" << checkPow<< " --- " ;
+                cout << "1: " << restaurants[counter]
+                << " or 2: " << restaurants[counter+ DISPLAYNEXT] << "? ";
+                string optionDelete;
+                const string FIRST = "1";
+                const string SECOND = "2";
+                const int goToSecond = 1;
+                
+                cin >>  optionDelete;
+                
+                
+                
+                if (optionDelete == FIRST ){
+                    
+                    restaurants.erase(restaurants.begin() + counter + goToSecond);
+                    break;
+                }
+                else if (optionDelete == SECOND){
+                    
+                    restaurants.erase(restaurants.begin() + counter);
+                    break;
+                }
+                else if ( cin.fail())
+                {
+                    cout << "Invalid response (" << optionDelete << ")" << endl;
+                    cin.clear();
+                    cin.ignore(numeric_limits<streamsize>::max(),'\n');
+                }
+                else{
+                    cout << "Invalid response (" << optionDelete << ")" << endl;
+                    
+                }
+                
+            }
+            
+        }
+        matchSize = (restaurants.size()/ getHalf)* toInt;
+        //resets the match size
+    }
+    cout << restaurants[START] << " is the winner!" << endl;
+    
+}
